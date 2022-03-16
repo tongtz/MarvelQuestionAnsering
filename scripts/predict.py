5082812003003a0c287b013b1b11e8824f327ffd
Claret <50113437+tongtz@users.noreply.github.com>
2022-03-16 14:11:42 -0400

    Create predict.py

---
+++ scripts/predict.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForQuestionAnswering
+
+model_checkpoint = "distilbert-base-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+bert_model = TFAutoModelForQuestionAnswering.from_pretrained('./Fine_tune_BERT/')
+
+def get_answer(context,question):
+  inputs = tokenizer([context], [question], return_tensors="np")
+  outputs = bert_model(inputs)
+  start_position = tf.argmax(outputs.start_logits, axis=1)
+  end_position = tf.argmax(outputs.end_logits, axis=1)
+  answer = inputs["input_ids"][0, int(start_position) : int(end_position) + 1]
+  return tokenizer.decode(answer)
+
