93c583e358c41bd347000b145d0395781bbac757
Claret <50113437+tongtz@users.noreply.github.com>
2022-03-16 15:04:36 -0400

    Update app.py

--- app.py
+++ app.py
@@ -74,6 +74,24 @@ st.markdown("## 🎈 Check answer ")
 
 st.header("")
 
+!pip install tensorflow
+import tensorflow as tf
+from transformers import AutoTokenizer
+from transformers import TFAutoModelForQuestionAnswering
+
+model_checkpoint = "distilbert-base-cased"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+
+bert_model = TFAutoModelForQuestionAnswering.from_pretrained('./Fine_tune_BERT/')
+
+def predict(context,question):
+  inputs = tokenizer([context], [question], return_tensors="np")
+  outputs = bert_model(inputs)
+  start_position = tf.argmax(outputs.start_logits, axis=1)
+  end_position = tf.argmax(outputs.end_logits, axis=1)
+  answer = inputs["input_ids"][0, int(start_position) : int(end_position) + 1]
+  return tokenizer.decode(answer)
+
 if submit_button:
 	st.markdown(predict(context,question))
 
